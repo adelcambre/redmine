b9d5229b27d7230cb7feb769c9ed10ca4a46cfc3
Andy Delcambre <adelcambre@gmail.com>
2015-11-02 13:27:12 -0700

    add_member method

--- lib/redmine/project.rb
+++ lib/redmine/project.rb
@@ -1,5 +1,14 @@
 module Redmine
   class Project < Base
+    def add_member(user)
+      user_id = if user.is_a?(Redmine::User)
+                  user.id
+                else
+                  user
+                end
+
+      post(:memberships, :membership => {:user_id => user_id, :role_ids => [3]})
+    end
   end
 end
 
